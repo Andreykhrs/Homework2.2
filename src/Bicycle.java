50f9c7e4a7a41adef900038e21bc59ce534e4847
Andreykhrs <andrey_khrs@mail.ru>
2024-04-05 11:26:29 +0800

    Homework is ready!

---
+++ src/Bicycle.java
@@ -0,0 +1,11 @@
+public class Bicycle extends Vehicles {
+
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void updateTyre() {
+        super.updateTyre();
+    }
+}
